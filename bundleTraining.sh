74a9acaee29312432d54d3a896eeb9b9a17570ec
Arnaud  Héritier <arnaud.heritier@exoplatform.com>
2010-09-29 21:09:48 +0000

    TRCRT-21 : Fix script when not called from local directory

--- bundleTraining.sh
+++ bundleTraining.sh
@@ -1,6 +1,6 @@
 #/bin/bash!
 
-WORKING_DIR=`cd ${0%/*}/exo-training-package; pwd`
+WORKING_DIR="`cd ${0%/*}; pwd`/exo-training-package"
 echo "Working directory : $WORKING_DIR"
 #rm -rf $WORKING_DIR
 mkdir -p $WORKING_DIR
@@ -8,21 +8,23 @@ mkdir -p $WORKING_DIR/repository
 mkdir -p $WORKING_DIR/apps
 mkdir -p $WORKING_DIR/binaries
 
+cd $WORKING_DIR/binaries/
 if [ ! -e $WORKING_DIR/binaries/jdk-6u21-linux-i586.bin ]; then
-wget -nc -nd -P $WORKING_DIR/binaries -r "http://cds.sun.com/is-bin/INTERSHOP.enfinity/WFS/CDS-CDS_Developer-Site/en_US/-/USD/VerifyItem-Start/jdk-6u21-linux-i586.bin?BundledLineItemUUID=7jSJ_hCuV.MAAAErwVYkgFh.&OrderID=kTGJ_hCuP3EAAAErrFYkgFh.&ProductID=LxaJ_hCy4mIAAAEpXLwzBGsB&FileName=/jdk-6u21-linux-i586.bin" -O jdk-6u21-linux-i586.bin
+  wget -nc -nd  -O jdk-6u21-linux-i586.bin -r "http://cds.sun.com/is-bin/INTERSHOP.enfinity/WFS/CDS-CDS_Developer-Site/en_US/-/USD/VerifyItem-Start/jdk-6u21-linux-i586.bin?BundledLineItemUUID=7jSJ_hCuV.MAAAErwVYkgFh.&OrderID=kTGJ_hCuP3EAAAErrFYkgFh.&ProductID=LxaJ_hCy4mIAAAEpXLwzBGsB&FileName=/jdk-6u21-linux-i586.bin"
 fi;
 if [ ! -e $WORKING_DIR/binaries/jdk-6u21-linux-x64.bin ]; then
-wget -nc -nd -P $WORKING_DIR/binaries -r "http://cds.sun.com/is-bin/INTERSHOP.enfinity/WFS/CDS-CDS_Developer-Site/en_US/-/USD/VerifyItem-Start/jdk-6u21-linux-x64.bin?BundledLineItemUUID=9XmJ_hCuJFAAAAEre2AkgFiC&OrderID=InqJ_hCusrEAAAEraWAkgFiC&ProductID=xKiJ_hCySHIAAAEpT7wzBGsB&FileName=/jdk-6u21-linux-x64.bin" -O jdk-6u21-linux-x64.bin
+  wget -nc -nd  -O jdk-6u21-linux-x64.bin -r "http://cds.sun.com/is-bin/INTERSHOP.enfinity/WFS/CDS-CDS_Developer-Site/en_US/-/USD/VerifyItem-Start/jdk-6u21-linux-x64.bin?BundledLineItemUUID=9XmJ_hCuJFAAAAEre2AkgFiC&OrderID=InqJ_hCusrEAAAEraWAkgFiC&ProductID=xKiJ_hCySHIAAAEpT7wzBGsB&FileName=/jdk-6u21-linux-x64.bin"
 fi;
 if [ ! -e $WORKING_DIR/binaries/jdk-6u21-windows-i586.exe ]; then
-wget -nc -nd -P $WORKING_DIR/binaries -r "http://cds.sun.com/is-bin/INTERSHOP.enfinity/WFS/CDS-CDS_Developer-Site/en_US/-/USD/VerifyItem-Start/jdk-6u21-windows-i586.exe?BundledLineItemUUID=Rt6J_hCuWyIAAAErxrYkgFiC&OrderID=56SJ_hCuB3cAAAErvLYkgFiC&ProductID=h2iJ_hCyKS4AAAEpVrwzBGsB&FileName=/jdk-6u21-windows-i586.exe" -O jdk-6u21-windows-i586.exe
+  wget -nc -nd -O jdk-6u21-windows-i586.exe -r "http://cds.sun.com/is-bin/INTERSHOP.enfinity/WFS/CDS-CDS_Developer-Site/en_US/-/USD/VerifyItem-Start/jdk-6u21-windows-i586.exe?BundledLineItemUUID=Rt6J_hCuWyIAAAErxrYkgFiC&OrderID=56SJ_hCuB3cAAAErvLYkgFiC&ProductID=h2iJ_hCyKS4AAAEpVrwzBGsB&FileName=/jdk-6u21-windows-i586.exe"
 fi;
 if [ ! -e $WORKING_DIR/binaries/jdk-6u21-windows-x64.exe ]; then
-wget -nc -nd -P $WORKING_DIR/binaries -r "http://cds.sun.com/is-bin/INTERSHOP.enfinity/WFS/CDS-CDS_Developer-Site/en_US/-/USD/VerifyItem-Start/jdk-6u21-windows-x64.exe?BundledLineItemUUID=NjOJ_hCuax8AAAErfPckgFiC&OrderID=46SJ_hCuEr4AAAErcfckgFiC&ProductID=BXiJ_hCyf.MAAAEpc7wzBGsB&FileName=/jdk-6u21-windows-x64.exe" -O jdk-6u21-windows-x64.exe
+  wget -nc -nd -O jdk-6u21-windows-x64.exe -r "http://cds.sun.com/is-bin/INTERSHOP.enfinity/WFS/CDS-CDS_Developer-Site/en_US/-/USD/VerifyItem-Start/jdk-6u21-windows-x64.exe?BundledLineItemUUID=NjOJ_hCuax8AAAErfPckgFiC&OrderID=46SJ_hCuEr4AAAErcfckgFiC&ProductID=BXiJ_hCyf.MAAAEpc7wzBGsB&FileName=/jdk-6u21-windows-x64.exe"
 fi;
 if [ ! -e $WORKING_DIR/binaries/apache-maven-2.2.1-bin.zip ]; then
-wget -nc -nd -P $WORKING_DIR/binaries -r "http://apache.cict.fr//maven/binaries/apache-maven-2.2.1-bin.zip"
+  wget -nc -nd -r "http://apache.cict.fr/maven/binaries/apache-maven-2.2.1-bin.zip"
 fi;
+cd -
 
 rm -rf $WORKING_DIR/apps/apache-maven-2.2.1
 unset M2_HOME
@@ -32,7 +34,7 @@ svn export --force ${0%/*}/exo-developer $WORKING_DIR/exo-developer
 
 function launchMaven {
   cd $1
-  $WORKING_DIR/apps/apache-maven-2.2.1/bin/mvn "org.apache.maven.plugins:maven-dependency-plugin:2.1:go-offline" clean install clean -Dmaven.repo.local=$WORKING_DIR/repository
+  $WORKING_DIR/apps/apache-maven-2.2.1/bin/mvn -B "org.apache.maven.plugins:maven-dependency-plugin:2.1:go-offline" clean install clean -Dmaven.repo.local=$WORKING_DIR/repository
   if [ "$?" -ne "0" ]; then
     echo "!!! Sorry, maven build failed. Packaging aborted. !!!"
     exit 1
@@ -47,5 +49,5 @@ function launchMaven {
 #launchMaven  $WORKING_DIR/exo-developer/220-customization/exercise-230-customization-40-site-creation/2-solution
 launchMaven  $WORKING_DIR/exo-developer/300-portlets/projects
 
-rm exo-training.zip
-zip -r exo-training.zip $WORKING_DIR/*
+rm ${0%/*}exo-training.zip
+zip -r ${0%/*}/exo-training.zip $WORKING_DIR/*
