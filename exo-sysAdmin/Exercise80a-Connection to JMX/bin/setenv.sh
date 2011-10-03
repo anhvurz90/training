6d23696d23518a9fb954b892b59a7258e96ca944
Gregory  Sébert <gsebert@exoplatform.com>
2011-10-03 16:13:51 +0000

    Add jmx

---
+++ exo-sysAdmin/Exercise80a-Connection to JMX/bin/setenv.sh
@@ -0,0 +1,1 @@
+export CATALINA_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=localhost";
